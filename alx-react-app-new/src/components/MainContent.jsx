241130389d92c77ca51da20b66d629bea94fd3f5
Feyisa Kumbi <feyisa025@gmail.com>
2025-07-18 10:20:32 +0300

    New updated app

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ background: 'lightblue', margin: '10px', padding: '10px'}}>
+            <p style={{color: 'blue', background: 'darkgray'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
