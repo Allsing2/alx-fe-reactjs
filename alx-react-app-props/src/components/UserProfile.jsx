cfcf06c540423cdfc7bee0848b0a6b86285c96fe
Feyisa Kumbi <feyisa025@gmail.com>
2025-07-19 11:39:01 +0300

    UserProfile.js is updated

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,20 @@
- const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
+// ✅ Added: [react] and [useContext]
+import React, { useContext } from 'react';
 
- export default UserProfile;
+// ✅ Added: [UserContext]
+import UserContext from '../UserContext';
+
+const UserProfile = () => {
+  // ✅ Consuming context using useContext
+  const user = useContext(UserContext); // [useContext], [UserContext]
+
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <p>Age: {user.age}</p>
+      <p>Bio: {user.bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
