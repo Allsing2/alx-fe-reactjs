9bc38f16e07881ac64325a3739f3c5f0f6343de4
Feyisa Kumbi <feyisa025@gmail.com>
2025-07-20 11:23:07 +0300

    Some styles applied

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -3,12 +3,16 @@ import { Link } from 'react-router-dom';
 function Navbar() {
   return (
     <nav style={{
-      backgroundColor: '#333',
-      padding: '10px',
-      display: 'flex',
-      gap: '20px',
-      position: 'relative',
-      top: '30px'
+        position: 'absolute',
+        top: '20px',
+        right: '30px',
+        backgroundColor: 'blue',
+        padding: '20px',
+        display: 'flex',
+        gap: '20px',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        borderBottomLeftRadius: '10px',
     }}>
       <Link to="./" style={{ color: '#fff' }}>Home</Link>
       <Link to="./about" style={{ color: '#fff' }}>About</Link>
