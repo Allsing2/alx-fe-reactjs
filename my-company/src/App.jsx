6b57ce695edcae74298d7ef42f4a32e54eb43c6a
Feyisa Kumbi <feyisa025@gmail.com>
2025-07-20 10:34:14 +0300

    Build a Simple Company Website with React

---
+++ my-company/src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
+import About from './Components/About';
+import Services from './Components/Services';
+import Contact from './Components/Contact';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="Components/" element={<Home />} />
+          <Route path="Components/about" element={<About />} />
+          <Route path="Components/services" element={<Services />} />
+          <Route path="Components/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+      <Home />
+      <About />
+      <Services />
+      <Contact />
+    </>
+  )
+}
+
+export default App
