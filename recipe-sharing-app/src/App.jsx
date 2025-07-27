82980dc84a910e792b6b78a6341d48ce2081f8b9
Feyisa Kumbi <feyisa025@gmail.com>
2025-07-27 21:24:42 +0300

    app updated

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Recipelist from './components/RecipeList'
+import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 
 function App() {
