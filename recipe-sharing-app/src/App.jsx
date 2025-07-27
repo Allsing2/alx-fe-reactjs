b86ede0ed7ab7f5bd543e564b829a41a13858446
Feyisa Kumbi <feyisa025@gmail.com>
2025-07-27 21:23:05 +0300

    RecipeList included

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import recipelist from './components/RecipeList'
+import Recipelist from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 
 function App() {
