3d7a14bc305433a2c62dd5fc88035617b3889a71
Feyisa Kumbi <feyisa025@gmail.com>
2025-07-27 22:02:53 +0300

    other edit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import RecipeList from './components/RecipeList'
 import AddRecipeForm from './components/AddRecipeForm'
 
+
 function App() {
   return (
     <div className="app">
